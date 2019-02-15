eb01c2e214dcf637b76c1baedf7e69dad56f8ff9
William Gulian <williamgulian@gmail.com>
2019-02-14 23:52:13 -0500

    move s100 bus operations to a namespace

--- Inc/bus_io.h
+++ Inc/bus_io.h
@@ -5,4 +5,18 @@
 #ifndef IMSAI8080STM32PROGRAMMER_BUS_IO_H
 #define IMSAI8080STM32PROGRAMMER_BUS_IO_H
 
+#include "Shift595.h"
+#include <stdint.h>
+
+namespace S100 {
+
+    extern bool bus_has_control;
+
+    void init();
+    void acquire();
+    void release();
+    void write_mem(uint16_t start_addr, uint8_t buf[], uintptr_t offset, uintptr_t size);
+
+};
+
 #endif //IMSAI8080STM32PROGRAMMER_BUS_IO_H
--- Src/cpp_main.cpp
+++ Src/cpp_main.cpp
@@ -2,7 +2,8 @@
 // Created by Will Gulian on 2019-02-14.
 //
 
-#include <Shift595.h>
+#include "bus_io.h"
+#include "Shift595.h"
 #include "cmsis_os.h"
 #include "cpp_main.h"
 
@@ -10,37 +11,21 @@ bus_pin led1{GPIOB, GPIO_PIN_0};
 bus_pin led2{GPIOB, GPIO_PIN_1};
 
 // active low
-bus_pin pin_addr_dsbl{GPIOB, GPIO_PIN_10};
-bus_pin pin_out_dsbl{GPIOB, GPIO_PIN_11};
-bus_pin pin_cc_dsbl{GPIOB, GPIO_PIN_8};
-
-Shift595 shift_addr{
-        {GPIOA, GPIO_PIN_2}, // srclk
-        {GPIOA, GPIO_PIN_3}, // rclk
-        {GPIOA, GPIO_PIN_1}, // ser
-        {GPIOA, GPIO_PIN_4}, // oe
-};
 
 void cpp_main() {
-    shift_addr.init();
-    shift_addr.set_enabled(true);
+    S100::init();
 
-    bus_write_pin(&pin_addr_dsbl, false);
-    bus_write_pin(&pin_out_dsbl, false);
-    bus_write_pin(&pin_cc_dsbl, false);
+    osDelay(500);
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wmissing-noreturn"
     while (true) {
-
-        uint8_t nums[] = {0xFF, 0};
 
         for (int i = 0; i < 2; i++) {
 
             bus_write_pin(&led1, i != 0);
             bus_write_pin(&led2, i == 0);
 
-            shift_addr.load_byte(nums[i]);
             osDelay(200);
         }
 
